d30623575de896e8ec8a9cac2c40a80edfba6498
vupham <vuquangpham2909@gmail.com>
2022-12-27 13:40:50 +0700

    :zap: add description for html from package.json

--- dev/script.js
+++ dev/script.js
@@ -19,6 +19,7 @@ const packageInfo = require('../package.json');
 const title = `${packageInfo.prettyName} v${packageInfo.version}`;
 document.title = `[DEV] ${title} - ${packageInfo.description}`;
 document.querySelector('[data-title]').innerHTML = title;
+document.querySelector('[data-description]').innerHTML = packageInfo.description;
 
 /**
  * Lib usage
