9d9f0f8d0e5014041e2bbaa4945e9ab46b723ddf
phucbm <hiphucbui@gmail.com>
2024-07-03 14:42:50 +0700

    refactor: convert configs.ts, helpers.ts

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,11 +1,12 @@
 import {ATTRS, CLASSES} from "./configs";
+import {Popup} from "./types"; // Assuming Popup type is defined in './types'
 
-export function initToggleTrigger(context){
+export function initToggleTrigger(context: Popup): void {
     // assign triggers via a[href="#id"], [toggle="id"]
     let triggerSelector = `a[href="#${context.id}"], [${ATTRS.toggle}="${context.id}"]`;
 
     // custom triggers
-    if(context.options.triggerSelector && context.options.triggerSelector.length){
+    if (context.options.triggerSelector && context.options.triggerSelector.length) {
         triggerSelector += `, ${context.options.triggerSelector}`;
     }
 
@@ -13,13 +14,13 @@ export function initToggleTrigger(context){
     assignToggle(context, document.querySelectorAll(triggerSelector));
 
     // any triggers without id inside this popup will also toggle this popup
-    assignToggle(context, context.outer.querySelectorAll(`[${ATTRS.toggle}]`));
+    assignToggle(context, context.outer!.querySelectorAll(`[${ATTRS.toggle}]`));
 }
 
-function assignToggle(context, triggers){
+function assignToggle(context: Popup, triggers: NodeListOf<Element>): void {
     triggers.forEach(trigger => {
         // avoid duplicate assign
-        if(trigger.classList.contains(CLASSES.triggerEnabled)) return;
+        if (trigger.classList.contains(CLASSES.triggerEnabled)) return;
 
         trigger.addEventListener('click', e => {
             e.preventDefault();
@@ -30,31 +31,34 @@ function assignToggle(context, triggers){
     });
 }
 
-
 /**
  * Wrap element
  * @param innerEl
  * @param outerEl
  * @returns {HTMLDivElement}
  */
-export function wrapElement(innerEl, outerEl = document.createElement('div')){
-    innerEl.parentNode.insertBefore(outerEl, innerEl);
+export function wrapElement(innerEl: HTMLElement, outerEl: HTMLDivElement = document.createElement('div')): HTMLDivElement {
+    innerEl.parentNode!.insertBefore(outerEl, innerEl);
     outerEl.appendChild(innerEl);
     return outerEl;
 }
-
 
 /**
  * Get scrollbar width
  * https://stackoverflow.com/a/986977/6453822
  * @returns {number}
  */
-export function getScrollbarWidth(){
+export function getScrollbarWidth(): number {
     // Creating invisible container
     const outer = document.createElement('div');
     outer.style.visibility = 'hidden';
     outer.style.overflow = 'scroll'; // forcing scrollbar to appear
-    outer.style.msOverflowStyle = 'scrollbar'; // needed for WinJS apps
+
+    // check if msOverflowStyle is supported
+    if ('msOverflowStyle' in outer.style) {
+        //@ts-ignore
+        outer.style.msOverflowStyle = 'scrollbar'; // needed for WinJS apps
+    }
     document.body.appendChild(outer);
 
     // Creating inner element and placing it in the container
@@ -65,7 +69,7 @@ export function getScrollbarWidth(){
     const scrollbarWidth = (outer.offsetWidth - inner.offsetWidth);
 
     // Removing temporary elements from the DOM
-    outer.parentNode.removeChild(outer);
+    outer.parentNode!.removeChild(outer);
 
     return scrollbarWidth;
 }
