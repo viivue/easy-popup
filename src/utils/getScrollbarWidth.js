8bc097e77ead1c321e676aa578f798a6856b6834
phucbm <hiphucbui@gmail.com>
2024-10-19 21:48:36 +0700

    feat(utils): improve getDetectedScrollbarWidth

--- src/utils/getScrollbarWidth.js
+++ src/utils/getScrollbarWidth.js
@@ -1,3 +1,5 @@
+import {isMobile} from "./isMobile";
+
 /**
  * To prevent page jump when popup is open, we add a padding-right to the body, this is the width of the scrollbar.
  * Scrollbar width is different between browsers and OS, so we need to calculate it.
@@ -5,6 +7,11 @@
  * So we allow the user to set the scrollbar width manually via the `scrollbarWidth` option.
  */
 export function getScrollbarWidth(context){
+    if(isMobile()){
+        // console.log('EasyPopup: Mobile or touch device detected, no need to calculate scrollbar width');
+        return 0;
+    }
+
     const {scrollbarWidth} = context.options;
 
     if(scrollbarWidth === undefined){
@@ -26,13 +33,25 @@ export function getScrollbarWidth(context){
  * https://stackoverflow.com/a/986977/6453822
  * @returns {number}
  */
-function getDetectedScrollbarWidth(){
-    // Creating invisible container
-    const outer = document.createElement('div');
+function getDetectedScrollbarWidth(outerElement = document.body){
+    // Use provided element or create a new div
+    const outer = outerElement || document.createElement('div');
+
+    // Store original styles
+    const originalStyles = {
+        visibility: outer.style.visibility,
+        overflow: outer.style.overflow,
+        msOverflowStyle: outer.style.msOverflowStyle
+    };
+
+    // Apply necessary styles
     outer.style.visibility = 'hidden';
     outer.style.overflow = 'scroll'; // forcing scrollbar to appear
     outer.style.msOverflowStyle = 'scrollbar'; // needed for WinJS apps
-    document.body.appendChild(outer);
+
+    if(!outerElement){
+        document.body.appendChild(outer);
+    }
 
     // Creating inner element and placing it in the container
     const inner = document.createElement('div');
@@ -41,8 +60,17 @@ function getDetectedScrollbarWidth(){
     // Calculating difference between container's full width and the child width
     const scrollbarWidth = (outer.offsetWidth - inner.offsetWidth);
 
-    // Removing temporary elements from the DOM
-    outer.parentNode.removeChild(outer);
+    // Cleaning up
+    outer.removeChild(inner);
+
+    if(!outerElement){
+        outer.parentNode.removeChild(outer);
+    }else{
+        // Restore original styles
+        outer.style.visibility = originalStyles.visibility;
+        outer.style.overflow = originalStyles.overflow;
+        outer.style.msOverflowStyle = originalStyles.msOverflowStyle;
+    }
 
     return scrollbarWidth;
 }
